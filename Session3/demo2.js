cb9ba9b662340abd00c534fb6f21ba3f6c5d32eb
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2021-09-04 18:11:04 +0530

    delete session3

--- Session3/demo2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var demo1_1 = require("./demo1");
-var obj1 = new demo1_1.Myclass("Sunil", "Ingale", 2);
-obj1.display();
-console.log(demo1_1.add(7, 7));
-console.log(demo1_1.pi);
