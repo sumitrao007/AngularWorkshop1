a0646316bd022964a2413be526fe2c04ff1de865
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2021-09-08 16:08:26 +0530

    Session8

---
+++ Angular/Session8/tdf/tdf.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrls: ['./tdf.component.css']
+})
+export class TdfComponent implements OnInit {
+
+  
+
+  contactMethod:any[]=[
+    {id:1,name:'Email'},
+    {id:2,name:'Phone'},
+    {id:3,name:'Office Phone'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSubmit(f){
+    console.log("Username "+f.value.myusername);
+    console.log("Radio button value "+f.value.con);
+  }
+
+}
